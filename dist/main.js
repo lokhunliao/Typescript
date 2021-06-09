57f66b4ea49c09cb2f2a6c8da46c412a4bf2c127
lokhunliao <lokhunnnn@gmail.com>
2021-06-09 00:11:59 -0700

    how to declare a function

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,8 @@
 var hello = 'world';
 //const can not be resign.
 //we can change value only to the same type. hello = true;
+var getFullName = function (name, surname) {
+    return name + ' ' + surname;
+};
+//It is safer to define everything Explicitly  
+console.log(getFullName('luke', 'liao'));
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,9 @@ let hello: string = 'world';
 
 //const can not be resign.
 //we can change value only to the same type. hello = true;
+
+const getFullName = (name: string, surname: string):string => {
+    return name + ' ' + surname;
+}
+//It is safer to define everything Explicitly  
+console.log(getFullName('luke', 'liao'));
